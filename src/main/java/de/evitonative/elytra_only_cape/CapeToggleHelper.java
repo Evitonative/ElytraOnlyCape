aebe1fef81511a85ed7436f5adca5c5d5ffe433a
Evito <47825249+Evitonative@users.noreply.github.com>
2026-06-25 21:33:11 +0200

    Added support for realms

--- src/main/java/de/evitonative/elytra_only_cape/CapeToggleHelper.java
+++ src/main/java/de/evitonative/elytra_only_cape/CapeToggleHelper.java
@@ -47,10 +47,10 @@ public class CapeToggleHelper {
             return false;
 
         Minecraft minecraft = Minecraft.getInstance();
-        boolean isInMenu = minecraft.player == null;
-        boolean isSinglePlayer = isSingleplayer(minecraft) && !isInMenu;
 
-        if (isSinglePlayer) return true;
+        boolean isInMenu = minecraft.player == null;
+        if (isInMenu) return false;
+        if (isSingleplayer(minecraft)) return true;
         if (env == ActiveEnvironment.SINGLEPLAYER) return false;
 
         ServerData currentServer = minecraft.getCurrentServer();
@@ -62,6 +62,8 @@ public class CapeToggleHelper {
                 .anyMatch(ip -> ip.toLowerCase().equals(currentIp))) return false;
 
         if (!ModConfig.instance.serverWhitelistEnabled) return true;
+
+        if (ModConfig.instance.whitelistRealms && currentServer.isRealm()) return true;
 
         return ModConfig.instance.serverWhitelist.stream()
                 .anyMatch(ip -> ip.toLowerCase().equals(currentIp));
